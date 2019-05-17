25d3463bd046a133faa9bed075ce4db8a73ec1c2
DebraJohn <debra.zhuang@zeasn.com>
2019-05-17 19:20:23 +0800

    回家再写

--- src/components/calc/index.js
+++ src/components/calc/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import './index.css'
+import './index.css';
+
+import Screen from '../screen';
 
 export default class Calc extends React.Component {
   render() {
     return (
-      <div className='calc'></div>
-    )
+      <div className="calc">
+        <Screen />
+      </div>
+    );
   }
 }
--- src/components/screen/index.js
+++ src/components/screen/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './index.css'
 
 export default class Screen extends React.Component {
   render() {
-    return <div class="screen"></div>
+    return <div className="screen">12</div>
   }
 }
