a23cf145d180f28429c69fce9b0d3a734917663b
DebraJohn <nihaozzihui@163.com>
2019-05-19 18:21:46 +0800

    change build path

---
+++ build/precache-manifest.4325725d94c3fbe2eeed27e1b4229a29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c74936bf9da1e5ebb491dc80c7baa8",
+    "url": "/calculator-react/build/index.html"
+  },
+  {
+    "revision": "bc890a9ff412807ad13b",
+    "url": "/calculator-react/build/static/css/main.9a5ce23f.chunk.css"
+  },
+  {
+    "revision": "8366164031ea33c798a0",
+    "url": "/calculator-react/build/static/js/2.73301c49.chunk.js"
+  },
+  {
+    "revision": "bc890a9ff412807ad13b",
+    "url": "/calculator-react/build/static/js/main.1b09b3e5.chunk.js"
+  },
+  {
+    "revision": "65b0c676ccc9b7cf49ec",
+    "url": "/calculator-react/build/static/js/runtime~main.ae6e74cb.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.07868e14343a0f491a3dbd735c29d014.js"
+  "/calculator-react/build/precache-manifest.4325725d94c3fbe2eeed27e1b4229a29.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/calculator-react/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
